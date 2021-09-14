e28b32ee35a90bb5fe76b1da1e5fa56c88b637c1
Adriene Smith <78666016+adrienesmith@users.noreply.github.com>
2021-09-14 07:18:54 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+function twoNumberSum(array, targetSum) {
+  const numbers = new Map();
+	const solution = new Map();
+	
+	for (let number of array) {
+		numbers.set(number, number);
+	}
+	
+	for (let number of array) {
+		const difference = targetSum - number;
+		if (difference !== number) {
+			if (numbers.has(difference)) {
+				solution.set(Math.min(number, difference), Math.max(number, difference));
+			}
+		}
+	}
+  
+	if (solution.size) {
+			return Array.from(...solution);
+	} else {
+		return [];
+	}
+}
